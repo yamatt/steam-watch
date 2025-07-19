4c47d7e5794d02f6cf6f48874d76761db72e19ef
Matt Copperwaite <matt@copperwaite.net>
2025-07-19 21:57:32 +0100

    lint fix

--- tests/schedule_parser/test_memory_db.py
+++ tests/schedule_parser/test_memory_db.py
@@ -10,8 +10,22 @@ class TestScheduleMemoryDB:
     def test_schedule(self):
         mock_schedule_parser = Mock(spec=ScheduleCollection)
         mock_schedule_parser.items = [
-            {"JsonScheduleV1":{"CIF_stp_indicator":"N","CIF_train_uid":"example-1","schedule_start_date":"2025-07-14","transaction_type":"Create"}},
-            {"JsonScheduleV1":{"CIF_stp_indicator":"N","CIF_train_uid":"example-2","schedule_start_date":"2025-07-14","transaction_type":"Delete"}},
+            {
+                "JsonScheduleV1": {
+                    "CIF_stp_indicator": "N",
+                    "CIF_train_uid": "example-1",
+                    "schedule_start_date": "2025-07-14",
+                    "transaction_type": "Create",
+                }
+            },
+            {
+                "JsonScheduleV1": {
+                    "CIF_stp_indicator": "N",
+                    "CIF_train_uid": "example-2",
+                    "schedule_start_date": "2025-07-14",
+                    "transaction_type": "Delete",
+                }
+            },
         ]
         schedule_memory_db = ScheduleMemoryDB(mock_schedule_parser)
 
