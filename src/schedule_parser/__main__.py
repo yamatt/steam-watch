749c327c027f65cafed7f8655042ba14a91b1e93
Matt Copperwaite <matt@copperwaite.net>
2025-07-20 09:26:50 +0100

    require at least one file

--- src/schedule_parser/__main__.py
+++ src/schedule_parser/__main__.py
@@ -4,10 +4,11 @@ from .collection import ScheduleCollection
 from .memory_db import ScheduleMemoryDB
 
 @click.command()
-@click.argument('file_paths', nargs=-1, type=click.Path(exists=True))
+@click.argument('file_paths', nargs=-1, type=click.Path(exists=True), required=True)
 def schedule(file_paths):
     schedule = ScheduleCollection.from_paths(file_paths)
     db = ScheduleMemoryDB(schedule)
+
 
 if __name__ == '__main__':
     schedule()
