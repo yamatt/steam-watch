12ae8dc78ba0a309b108e64e0c1dbc3d353e9c86
Matt Copperwaite <matt@copperwaite.net>
2025-07-20 18:12:42 +0100

    create date and signal views of schedule data

--- src/schedule_parser/memory_db.py
+++ src/schedule_parser/memory_db.py
@@ -13,7 +13,7 @@ class ScheduleMemoryDB:
         self.coll = coll
     
     @cached_property
-    def services(self) -> list[dict]:
+    def services(self) -> OrderedDict:
         db = OrderedDict()
         for item in self.coll.items:
             if "JsonScheduleV1" in item:
@@ -23,3 +23,21 @@ class ScheduleMemoryDB:
                 if schedule_item["transaction_type"] == "Delete":
                     db.pop((schedule_item["schedule_start_date"], schedule_item["CIF_train_uid"], schedule_item["CIF_stp_indicator"]), None)
         return db
+
+    @cached_property
+    def services_by_date(self) -> OrderedDict:
+        services = OrderedDict()
+        for key, service in self.services.items():
+            new_service = services.get(key[0], [])
+            new_service.append(service)
+            services[key[0]] = new_service
+        return services
+
+    @cached_property
+    def services_by_signal_id(self) -> OrderedDict:
+        services = OrderedDict()
+        for _, service in self.services.items():
+            new_service = services.get(service["schedule_segment"]["signalling_id"], [])
+            new_service.append(service)
+            services[service["schedule_segment"]["signalling_id"]] = new_service
+        return services
--- tests/schedule_parser/test_memory_db.py
+++ tests/schedule_parser/test_memory_db.py
@@ -5,36 +5,60 @@ import pytest
 from schedule_parser.collection import ScheduleCollection
 from schedule_parser.memory_db import ScheduleMemoryDB
 
+@pytest.fixture
+def mock_schedule_collection():
+    mock_schedule_parser = Mock(spec=ScheduleCollection)
+    mock_schedule_parser.items = [
+        {
+            "JsonScheduleV1": {
+                "CIF_stp_indicator": "N",
+                "CIF_train_uid": "example-1",
+                "schedule_start_date": "2025-07-14",
+                "transaction_type": "Create",
+                "schedule_segment": {
+                    "signalling_id": "1Z40"
+                }
+            }
+        },
+        {
+            "JsonScheduleV1": {
+                "CIF_stp_indicator": "N",
+                "CIF_train_uid": "example-2",
+                "schedule_start_date": "2025-07-14",
+                "transaction_type": "Delete",
+                "schedule_segment": {
+                    "signalling_id": "1Z40"
+                }
+            }
+        },
+    ]
+    return mock_schedule_parser
 
 class TestScheduleMemoryDB:
-    def test_schedule(self):
-        mock_schedule_parser = Mock(spec=ScheduleCollection)
-        mock_schedule_parser.items = [
-            {
-                "JsonScheduleV1": {
-                    "CIF_stp_indicator": "N",
-                    "CIF_train_uid": "example-1",
-                    "schedule_start_date": "2025-07-14",
-                    "transaction_type": "Create",
-                }
-            },
-            {
-                "JsonScheduleV1": {
-                    "CIF_stp_indicator": "N",
-                    "CIF_train_uid": "example-2",
-                    "schedule_start_date": "2025-07-14",
-                    "transaction_type": "Delete",
-                }
-            },
-        ]
-        schedule_memory_db = ScheduleMemoryDB(mock_schedule_parser)
+    def test_services(self, mock_schedule_collection):
+        
+        schedule_memory_db = ScheduleMemoryDB(mock_schedule_collection)
 
         services = schedule_memory_db.services
 
         assert len(services) == 1
-        assert services[("2025-07-14", "example-1", "N")] == {
-            "transaction_type": "Create",
-            "schedule_start_date": "2025-07-14",
-            "CIF_train_uid": "example-1",
-            "CIF_stp_indicator": "N",
-        }
+        assert ("2025-07-14", "example-1", "N") in services
+
+    def test_services_by_date(self, mock_schedule_collection):
+
+        schedule_memory_db = ScheduleMemoryDB(mock_schedule_collection)
+
+        services = schedule_memory_db.services_by_date
+
+        assert len(services) == 1
+        assert "2025-07-14" in services
+
+    def test_services_by_date(self, mock_schedule_collection):
+
+        schedule_memory_db = ScheduleMemoryDB(mock_schedule_collection)
+
+        services = schedule_memory_db.services_by_signal_id
+
+        assert len(services) == 1
+        assert "1Z40" in services
+
