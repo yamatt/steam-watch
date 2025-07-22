e7e64a7dd3fad2f2e33b645d818f2fbdef17b4ec
Matt Copperwaite <matt@copperwaite.net>
2025-07-22 22:24:39 +0100

    adds a very inaccurate train capitalisation converter

--- src/message_render/__main__.py
+++ src/message_render/__main__.py
@@ -9,6 +9,13 @@ def datetime_to_google_calendar_format(dt_str):
     dt = datetime.fromisoformat(dt_str)
     return dt.strftime("%Y%m%dT%H%M%SZ")
 
+def capitalise(s):
+    def cap(part):
+        if len(part) == 3:
+            return part
+        return part.capitalize()
+    return " ".join([ cap(part) for part in s.split() ])
+
 @click.command()
 @click.argument("template_file_name", required=True)
 @click.argument("data_file_path", type=click.Path(exists=True), required=True)
@@ -18,6 +25,7 @@ def render(template_file_name, data_file_path, tiploc_data_file_path, templates_
     env = Environment(loader=FileSystemLoader(templates_dir_path))
     env.filters["urlquote"] = quote_plus
     env.filters["dt"] = datetime_to_google_calendar_format
+    env.filters["capitalise"] = capitalise
     template = env.get_template(template_file_name)
     output = template.render(data=json.load(open(data_file_path)), tiploc=json.load(open(tiploc_data_file_path)))
     print(output)
